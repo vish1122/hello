d7cc9bd9a31f12cb8adbdaec7bf1bd5cc266d69b
vish1122 <32099628+vish1122@users.noreply.github.com>
2018-06-15 16:02:07 -0400

    Add files via upload

---
+++ airbnb proj/global.R
@@ -0,0 +1,2 @@
+library(data.table)
+rlisting <- fread(file = "~/Desktop/project datasets/listings.csv")
---
+++ airbnb proj/ui.R
@@ -0,0 +1,13 @@
+library(shiny)
+fluidPage(
+  titlePanel("Airbnb For NYC")
+#sidebarLayout(
+ # sidebarPanel(
+  #  sliderInput("bins","Number of bins:",
+   #             min=1,max = 50,value = 30)
+  #),
+ # mainPanel (
+#  plotOutput("distPlot")
+  #)
+  
+)
